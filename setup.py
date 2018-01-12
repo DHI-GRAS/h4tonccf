eaf515ea5eebf5d01ee79a15a9b019275e780827
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-12 15:00:29 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='h4tonccf',
-    version='0.1',
+    version='0.2',
     description='Python wrapper for h4tonccf',
     author='Jonas Solvsteen',
     author_email='josl@dhigroup.com',
